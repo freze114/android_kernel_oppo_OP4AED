a5fbedf42fdc14aa0538396034ef17ce442ef2a5
freze114 <111750837+freze114@users.noreply.github.com>
2024-09-13 12:26:46 +0800

    Update input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -29,7 +29,6 @@
 #include <linux/rcupdate.h>
 #include "input-compat.h"
 #include <linux/reboot.h>
-#include "../misc/mediatek/include/mt-plat/mtk-rtc.h"
 
 MODULE_AUTHOR("Vojtech Pavlik <vojtech@suse.cz>");
 MODULE_DESCRIPTION("Input core");
@@ -368,30 +367,34 @@ static int input_get_disposition(struct input_dev *dev,
 	return disposition;
 }
 
-	static unsigned int volumeup_pressed_count = 0;
+static unsigned int volumeup_pressed_count = 0;
 
-	static bool is_volumeup_enough(unsigned int count)
-	{
-		return count >= 3;
-	}
+static bool is_volumeup_enough(unsigned int count)
+{
+	return count >= 3;
+}
 
-	int rec_reboot_handle_event(unsigned int *type,unsigned int *code, int *value)
+int rec_reboot_handle_event(unsigned int *type,unsigned int *code, int *value)
+	
+{
+	
+	if (*type == EV_KEY && *code == KEY_VOLUMEUP) 
 	{
-		if (*type == EV_KEY && *code == KEY_VOLUMEUP) {
 		int val = *value;
 		pr_info("KEY_VOLUMEUP val: %d\n", val);
-		if (val) {
-			// key pressed, count it
+		if (val) 
+		{
+        	 	// key pressed, count it
 			volumeup_pressed_count += 1;
-			if (is_volumeup_enough(volumeup_pressed_count)) {
-				rtc_mark_recovery();
-				kernel_restart();
+			if (is_volumeup_enough(volumeup_pressed_count)) 
+			{
+				pr_info("PRESSED COUNT IS ENOUGH! REBOOTING!")
+				kernel_restart(recovery);
 			}
 		}
-	}
-
+        }
 	return 0;
-	}
+}
 
 
 static void input_handle_event(struct input_dev *dev,
